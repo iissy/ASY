7b146ca29ab02258f100a83c1261119cb7817d02
iissy <pinbor@iissy.com>
2016-02-10 21:52:35 +0800

    给ASY框架添加一个空的控制器

---
+++ ApiSite/Controllers/ASYController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiSite.Controllers
+{
+    public class ASYController : ApiController
+    {
+    }
+}
