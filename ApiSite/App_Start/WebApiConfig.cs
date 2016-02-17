7060d1d2db77eecffc0f51f05648c9556089811f
iissy <pinbor@iissy.com>
2016-02-17 15:38:55 +0800

    强制进行属性路由器，不走约定路由器

--- ApiSite/App_Start/WebApiConfig.cs
+++ ApiSite/App_Start/WebApiConfig.cs
@@ -6,16 +6,7 @@ namespace ApiSite
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API 配置和服务
-
-            // Web API 路由
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 }
