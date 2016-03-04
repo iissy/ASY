55f44d6c20f330b66fa546a14cb695c00a1af15c
iissy <pinbor@iissy.com>
2016-03-04 20:44:54 +0800

    更新升级组建

--- ApiSite/DependencyResolution/StructureMapDependencyScope.cs
+++ ApiSite/DependencyResolution/StructureMapDependencyScope.cs
@@ -82,16 +82,14 @@ namespace ApiSite.DependencyResolution {
         }
 
         public void Dispose() {
-            if (CurrentNestedContainer != null) {
-                CurrentNestedContainer.Dispose();
-            }
-
+            DisposeNestedContainer();
             Container.Dispose();
         }
 
         public void DisposeNestedContainer() {
             if (CurrentNestedContainer != null) {
                 CurrentNestedContainer.Dispose();
+				CurrentNestedContainer = null;
             }
         }
 
