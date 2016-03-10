1295991cb5ecdbe749013d855b9c2ac18b45bf0e
iissy <pinbor@iissy.com>
2016-03-10 17:18:26 +0800

    添加一个实体的上传类

---
+++ Util/UploadHelpers/UploadObject.cs
@@ -0,0 +1,9 @@
+﻿namespace ASY.Iissy.Util.UploadHelpers
+{
+    public class UploadObject
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public string Url { get; set; }
+    }
+}
